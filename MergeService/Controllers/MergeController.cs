dc8d18336eb3921b19793928b0e4bc4e79db89cd
AmenaRafiq <amena.rafiq@avanade.com>
2021-06-09 17:03:17 +0100

    Add merge controller

---
+++ MergeService/Controllers/MergeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MergeService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MergeController : ControllerBase
+    {
+        private IConfiguration Configuration;
+        public MergeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var daysService = $"{Configuration["daysServiceURL"]}/days";
+            //var daysService = $"http://localhost:17829/days";
+            var daysServiceResponseCall = await new HttpClient().GetStringAsync(daysService);
+
+            var monthService = $"{Configuration["monthServiceURL"]}/month";
+            //var monthService = $"http://localhost:44717/month";
+            var monthServiceResponseCall = await new HttpClient().GetStringAsync(monthService);
+
+            var mergedResponse = $"{monthServiceResponseCall}{daysServiceResponseCall}";
+            return Ok(mergedResponse);
+        }
+    }
+}
