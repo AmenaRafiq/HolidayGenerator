a4a67c25913502e41e7317c2946d1f922128743e
AmenaRafiq <amena.rafiq@avanade.com>
2021-06-17 15:11:37 +0100

    Add months test

---
+++ HolidayGeneratorTest/MonthControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using MonthService.Controllers;
+using System;
+using Xunit;
+
+namespace HolidayGeneratorTest
+{
+    public class MonthControllerTest
+    {
+        public MonthController monthContoller;
+
+        [Fact]
+        public void Get_Test()
+        {
+            //Arrange
+            monthContoller = new MonthController();
+            //Act
+            var month = monthContoller.Get();
+
+            //Assert
+            Assert.True((month.GetType()).Equals(typeof(Months)));
+           
+        }
+    }
+}
